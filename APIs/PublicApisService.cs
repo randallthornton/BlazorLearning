3ea718ed19cff38c31f9a9b0a4332cebc4fc8fc6
Randall Thornton <Randall.Thornton@cc-racks.com>
2024-05-18 18:13:21 -0400

    Added cat status code image lookup!

--- APIs/PublicApisService.cs
+++ APIs/PublicApisService.cs
@@ -5,29 +5,40 @@ namespace BlazorWebApp1.APIs;
 
 public class PublicApisService
 {
-	HttpClient client;
-	string meowFactsUrl = "https://meowfacts.herokuapp.com/";
+    HttpClient client;
+    string meowFactsUrl = "https://meowfacts.herokuapp.com/";
 
-	public PublicApisService(HttpClient client)
-	{
-		this.client = client;
-	}
+    public PublicApisService(HttpClient client)
+    {
+        this.client = client;
+    }
 
-	public async Task<CatFactsResponse?> GetMeowFacts(int count)
-	{
-		// call 
+    public async Task<CatFactsResponse?> GetMeowFacts(int count)
+    {
+        // call 
 
-		var result = await client.GetAsync(meowFactsUrl + $"?count={count}");
+        var result = await client.GetAsync(meowFactsUrl + $"?count={count}");
 
-		result.EnsureSuccessStatusCode();
+        result.EnsureSuccessStatusCode();
 
-		var content = await result.Content.ReadFromJsonAsync<CatFactsResponse>();
+        var content = await result.Content.ReadFromJsonAsync<CatFactsResponse>();
 
-		return content;
-	}
+        return content;
+    }
+
+    public async Task<byte[]> GetStatusCodeImage(int statusCode)
+    {
+        var result = await client.GetAsync($"https://http.cat/{statusCode}");
+
+        result.EnsureSuccessStatusCode();
+
+        var stream = await result.Content.ReadAsByteArrayAsync();
+
+        return stream;
+    }
 }
 
 public class CatFactsResponse
 {
-	public string[] Data { get; set; }
+    public string[] Data { get; set; }
 }
