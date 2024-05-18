3278a0fee6ddaec5593eb1e9bb2ca624f706c821
Randall Thornton <randall.thornton@universalorlando.com>
2024-05-18 09:55:06 -0400

    Un-nested files to prepare for tailwindcss integratrion

--- BlazorWebApp1.Client/Program.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-builder.Services.AddMudBlazorJsApi();
-
-await builder.Build().RunAsync();
